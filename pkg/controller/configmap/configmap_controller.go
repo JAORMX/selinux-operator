ea0596a7e159acc1e4926fdcbdcd8d434d917bdf
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2019-12-20 01:25:30 +0000

    Remove placeholder comment
    I forgot to remove this.

--- pkg/controller/configmap/configmap_controller.go
+++ pkg/controller/configmap/configmap_controller.go
@@ -23,11 +23,6 @@ import (
 
 var log = logf.Log.WithName("controller_configmap")
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
 // Add creates a new ConfigMap Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
